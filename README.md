97778ec152268fff2a3bd4cd05fc6843886ea729
Francisco53 <franciscolima2228648@gmail.com>
2023-08-10 00:05:52 -0300

    Configuração do projeto e método cadastrar produto

---
+++ README.md
@@ -0,0 +1,14 @@
+<h2 align="center">SQUAD 1</h2>
+
+## Intregrantes:
+- Francisco
+- Bruno
+- Josué
+- Juvenal
+
+## Distribuição das questões:
+
+Francisco: [Configuração do projeto e o método cadastrar produto]  
+Bruno: [3]  
+Josué: [4]  
+Juvenal: [5]
---
+++ src/main/java/br/com/cursopcv/respositorio/RepositorioProduto.java
@@ -0,0 +1,35 @@
+package br.com.cursopcv.respositorio;
+
+import br.com.cursopcv.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+public class RepositorioProduto {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    public RepositorioProduto(){
+        emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        em = emf.createEntityManager();
+    }
+
+    public void salvar(Produto produto){
+        try {
+            em.getTransaction().begin();
+            em.merge(produto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    public void fecharEntityManager() {
+        em.close();
+    }
+}
